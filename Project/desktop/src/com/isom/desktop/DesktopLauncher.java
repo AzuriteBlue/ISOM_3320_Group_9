3dac2a2b00cd06c5fe8401245381531289901627
Ryan Li <ryanxli@outlook.com>
2017-01-23 15:32:58 +0800

    First upload.
    Uploaded skeleton of the project.

---
+++ Project/desktop/src/com/isom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.isom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.isom.WikiJump;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WikiJump(), config);
+	}
+}
